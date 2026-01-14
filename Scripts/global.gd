1e30a0d6ddccbe010f621fcc446d7c3764bbe3b9
AbeCoder2011 <158455019+AbeCoder2011@users.noreply.github.com>
2026-01-14 13:43:32 +0100

    global

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -16,9 +16,29 @@ const BOAT_STATS = {
 	},"canoe":{
 		"speed":4,
 		"turn_speed":5,
-		"hp":2,
+		"hp":5,
 		"coin_multiplier":1.5
-	},
+	}, "basic_sailboat":{
+		"speed":6,
+		"turn_speed":3,
+		"hp":7,
+		"coin_multiplier":2.0
+	}, "improved_sailboat":{
+		"speed":7,
+		"turn_speed":4,
+		"hp":8,
+		"coin_multiplier":2.5
+	}, "basic_motorboat":{
+		"speed":9,
+		"turn_speed":4,
+		"hp":10,
+		"coin_multiplier":3.0
+	}, "reinforced_motorboat":{
+		"speed":9,
+		"turn_speed":3,
+		"hp":15,
+		"coin_multiplier":3.5
+	}
 }
 
 var LevelName = ""
